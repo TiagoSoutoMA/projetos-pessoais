ffc8e7b4a7e11dd563b10fa76a7f0a3cd2b96eaf
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2023-10-09 10:49:18 -0300

    Criando classes de cenário, identificando os lutadores

--- luta/assets/js/script.js
+++ luta/assets/js/script.js
@@ -1,3 +1,8 @@
-let char = new TeamHarry("Harry Potter");
+let charH = new TeamHarry("Harry Potter");
+let charV = new TeamVoldemort("Voldemort");
 
+const stage = new Stage(
+    charH, charV, document.querySelector("#char"), document.querySelector("#monster")
+)
 
+stage.start();
