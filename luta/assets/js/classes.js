ad196d1c92982d46b0c45fc1bf8650eae6495e14
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2023-10-09 11:20:40 -0300

    Ataques criados

--- luta/assets/js/classes.js
+++ luta/assets/js/classes.js
@@ -74,12 +74,12 @@ class Stage {
 
     update() {
         // Fighter 1
-        this.fighter1El.querySelector(".name").innerHTML = `${this.fighter1.name} - ${this.fighter1.life} HP`;
+        this.fighter1El.querySelector(".name").innerHTML = `${this.fighter1.name} - ${this.fighter1.life.toFixed(1)} HP`;
         let f1Pct = (this.fighter1.life / this.fighter1.maxLife) * 100;
         this.fighter1El.querySelector(".bar").style.width = `${f1Pct}%`;
 
         // Fighter 2
-        this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life} HP`;
+        this.fighter2El.querySelector(".name").innerHTML = `${this.fighter2.name} - ${this.fighter2.life.toFixed(1)} HP`;
         let f2Pct = (this.fighter2.life / this.fighter2.maxLife) * 100;
         this.fighter2El.querySelector(".bar").style.width = `${f2Pct}%`;
     }
@@ -89,6 +89,19 @@ class Stage {
             console.log("atacando morto");
             return;
         } 
+
+        let attackFactor = (Math.random() * 2).toFixed(2);
+        let desenseFactor = (Math.random() * 2).toFixed(2);
+
+        let actualAttack = attacking.attack * attackFactor;
+        let actualDefense = attacked.defense * desenseFactor;
+
+        if(actualAttack > actualDefense) {
+            attacked.life -= actualAttack;
+            console.log(`${attacking.name} causou ${actualAttack.toFixed(2)} de dano em ${attacked.name}`)
+        } else {
+            console.log(`${attacked.name} conseguiu defender`);
+        }
 
         this.update();
     }
