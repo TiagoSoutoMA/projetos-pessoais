e4b40ae5c86fce7b25e44e0c461c99a3872b48a3
TiagoFacisa <tiago.maior@maisunifacisa.com.br>
2023-10-09 11:31:11 -0300

    Adiciona as ações da luta dentro do log

--- luta/assets/js/script.js
+++ luta/assets/js/script.js
@@ -1,8 +1,10 @@
+let log = new Log(document.querySelector(".log"));
+
 let charH = new TeamHarry("Harry Potter");
 let charV = new TeamVoldemort("Voldemort");
 
 const stage = new Stage(
-    charH, charV, document.querySelector("#char"), document.querySelector("#monster")
+    charH, charV, document.querySelector("#char"), document.querySelector("#monster"), log
 )
 
 stage.start();
